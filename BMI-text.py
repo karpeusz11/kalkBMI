229dfeb6166f9db562b6eef77fd18a33961a78b8
JJMika97 <148628575+JJMika97@users.noreply.github.com>
2023-12-10 18:18:08 +0100

    Add files via upload

---
+++ BMI-text.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def BMI(waga, wzrost):
+    return waga / (wzrost / 100) ** 2
+
+def main():
+        df = pd.read_excel(r'C:\Users\janmi\Desktop\XD.xlsx')
+        df['BMI'] = df['Waga'] / (df['Wzrost'] / 100) ** 2
+        print(df[['Waga', 'Wzrost', 'BMI']])
+
+main()
